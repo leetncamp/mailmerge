534763131785145951f536926ea96e5a82488082
Lee Campbell <lee@salk.edu>
2016-05-18 12:27:08 -0700

    Updated build

--- osxBuild.sh
+++ osxBuild.sh
@@ -2,4 +2,4 @@
 rm -rf build dist send.command
 pyinstaller send.py -F --hidden-i Cookie  --hidden-i django.template.defaulttags --hidden-i django.template.loader_tags
 mv dist/send send.command
-rm -rf build dist
+rm -rf build dist send.spec
