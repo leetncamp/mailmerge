98ee5b28084f6ec214747377e5f933ab597b1103
Lee Campbell <lee@salk.edu>
2020-04-13 14:18:04 -0700

    send.command working as a binary

--- osxBuild.sh
+++ osxBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf build dist send.command
-pyinstaller send.py -F --hidden-i Cookie  --hidden-i django.template.defaulttags --hidden-i django.template.loader_tags
+pyinstaller send.py -F --hidden-i Cookie  --hidden-i django.template.defaulttags --hidden-i django.template.loader_tags --hidden-import pkg_resources.py2_warn 
 mv dist/send send.command
 rm -rf build dist send.spec
